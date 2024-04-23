d8b0caad6a1382646da9b1cfb679dbc2bc7052d1
vzwarbee <dovantai112344@gmail.com>
2024-04-23 14:42:15 +0700

    first commit

---
+++ test-chuyen-huong.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bạn đã bị bịp hehe</title>
+    <meta property="og:locale" content=""/>
+    <meta property="og:description" content="" />
+    <meta property="og:description" content="" />
+    <meta property="og:title" content="" />
+    <meta property="og:url" content="https://top10tphcm.com/wp-content/uploads/2023/02/gai-dep-nhat-viet-nam-1.jpg"/>
+    <meta property="og:image" content="https://top10tphcm.com/wp-content/uploads/2023/02/gai-dep-nhat-viet-nam-1.jpg"/>
+</head>
+<body>
+    <script>
+        window.location.replace()
+    </script>
+</body>
+</html>
