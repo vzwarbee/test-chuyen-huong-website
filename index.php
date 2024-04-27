9ff3ad1dd4cef73f40d2e1bfb0727e51307226f1
vzwarbee <dovantai112344@gmail.com>
2024-04-27 13:50:24 +0700

    qưeasd

--- index.php
+++ index.php
@@ -2,16 +2,15 @@
 <html lang="en">
   <head>
     <meta property="og:locale" content="" />
-    <meta property="og:description" content="" />
-    <meta property="og:description" content="" />
-    <meta property="og:title" content="" />
+    <meta property="og:description" content="Vụ án chấn động, phân xác nạn nhân tên Công lỏ, ỏ vào vali và trả trôi ở dưới sông" />
+    <meta property="og:title" content="Vụ án phân xác Công lỏ, bỏ vào vali" />
     <meta
       property="og:url"
-      content="https://top10tphcm.com/wp-content/uploads/2023/02/gai-dep-nhat-viet-nam-1.jpg"
+      content="https://vcdn-dulich.vnecdn.net/2018/03/10/28879911-10209937815515201-448-3104-3317-1520669287.jpg"
     />
     <meta
       property="og:image"
-      content="https://top10tphcm.com/wp-content/uploads/2023/02/gai-dep-nhat-viet-nam-1.jpg"
+      content="https://vcdn-dulich.vnecdn.net/2018/03/10/28879911-10209937815515201-448-3104-3317-1520669287.jpg"
     />
   </head>
   <body>
