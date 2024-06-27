1b5c6ea20bb93010447b62fbd855ec34410db611
loaf <85181398+vzwarbee@users.noreply.github.com>
2024-06-27 12:16:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,19 +2,10 @@
 <html lang="en">
   <head>
     <meta property="og:locale" content="" />
-    <meta property="og:description" content="Vụ án chấn động, phân xác nạn nhân tên Công lỏ, ỏ vào vali và trả trôi ở dưới sông" />
-    <meta property="og:title" content="Vụ án phân xác Công lỏ, bỏ vào vali" />
-    <meta
-      property="og:url"
-      content="https://vcdn-dulich.vnecdn.net/2018/03/10/28879911-10209937815515201-448-3104-3317-1520669287.jpg"
-    />
-    <meta
-      property="og:image"
-      content="https://vcdn-dulich.vnecdn.net/2018/03/10/28879911-10209937815515201-448-3104-3317-1520669287.jpg"
-    />
+    
   </head>
   <body>
     <h1 id="bip">Hello world!!</h1>
-    <script type="text/javascript" src="https://s.skimresources.com/js/265672X1750766.skimlinks.js"></script>
+    <script type="text/javascript" src="./app.js"></script>
   </body>
 </html>
